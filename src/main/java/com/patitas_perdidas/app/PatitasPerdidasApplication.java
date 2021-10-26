b4483d4cd988c5bedb928b0840e52af53da6f916
Juan Pablo Ayala <lozziajuan@gmail.com>
2021-10-25 21:17:15 -0300

    Primer commit: proyecto vacio

---
+++ src/main/java/com/patitas_perdidas/app/PatitasPerdidasApplication.java
@@ -0,0 +1,13 @@
+package com.patitas_perdidas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasPerdidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasPerdidasApplication.class, args);
+	}
+
+}
