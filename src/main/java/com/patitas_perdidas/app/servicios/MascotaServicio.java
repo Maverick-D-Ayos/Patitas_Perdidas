9064822ee7d639b01d522789d6cb04fddefa751c
Fabian Sassola <fabiasass_13@hotmail.com>
2021-10-27 20:59:16 -0300

    Actulizacion MascotaServicio, PersonaServicio. Se agregan paquete de excepciones y clases correspondientes

--- src/main/java/com/patitas_perdidas/app/servicios/MascotaServicio.java
+++ src/main/java/com/patitas_perdidas/app/servicios/MascotaServicio.java
@@ -8,9 +8,12 @@ import javax.mail.Multipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.patitas_perdidas.app.entidades.Mascota;
+import com.patitas_perdidas.app.excepciones.MascotaExcepcion;
 import com.patitas_perdidas.app.repositorios.MascotaRepositorio;
 
 @Service
@@ -21,6 +24,7 @@ public class MascotaServicio {
 	@Autowired
 	private FotoServicio fs;
 	
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
 	public void crearMascota(String nombre, String descripcion, String color, String raza, String tamaño,
 			Boolean encontrado, Date fecha, String especie, String zona, MultipartFile archivo) {
 		Mascota m=new Mascota();
@@ -40,13 +44,15 @@ public class MascotaServicio {
 		mr.save(m);	
 	}
 	
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
 	public void eliminarMascota(String id) {
 		Mascota m=mr.getById(id);
 		mr.delete(m);
 	}
 	
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
 	public void modificarMascota(String id,String nombre, String descripcion, String color, String raza, String tamaño,
-			Boolean encontrado, Date fecha, String especie, Zona zona, MultipartFile archivo) {
+			Boolean encontrado, Date fecha, String especie, String zona, MultipartFile archivo) throws MascotaExcepcion {
 		Mascota m=mr.getById(id);
 		
 		m.setNombre(nombre);
@@ -64,18 +70,35 @@ public class MascotaServicio {
 		mr.save(m);	
 	}
 	
-	public List<Mascota> listarMascotas(){
+	@Transactional(readOnly = true)
+	public List<Mascota> listarTodasMascotas(){
 		List<Mascota> lm= mr.findAll();
 		return lm;
 	}
+	@Transactional(readOnly = true)
+	public List<Mascota> listarMascotasActivasPerdidas(){
+		List<Mascota> lm= mr.buscarListaPerdidos();
+		return lm;
+	}
+	@Transactional(readOnly = true)
+	public List<Mascota> listarMascotasPorRaza(String raza){
+		List<Mascota> lm= mr.buscarListaRaza(raza);
+		return lm;
+	}
+	@Transactional(readOnly = true)
+	public List<Mascota> listarMascotasColor(String color){
+		List<Mascota> lm= mr.buscarListaColor(color);
+		return lm;
+	}
 	
-	public Mascota buscaPorId(String id) throws Exception{
+	@Transactional(readOnly = true)
+	public Mascota buscaPorId(String id) throws MascotaExcepcion{
 		Optional<Mascota> oMascota=mr.findById(id);
 		if(oMascota.isPresent()) {
 			Mascota mascota = oMascota.get();
 			return mascota;
 		}else {
-			throw new Exception("No se encuentra la mascota");
+			throw new MascotaExcepcion("No se encuentra la mascota");
 		}
 	}
 	
