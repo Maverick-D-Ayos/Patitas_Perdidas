b10732e048af9aba9fbcb86f8620aab560eb710d
Juan Pablo Ayala <lozziajuan@gmail.com>
2021-11-02 19:12:10 -0300

    Eliminacion de Foto, FotoServicio, cambios en formperdido, Entidad mascota, su servici y, controlador

--- src/main/java/com/patitas_perdidas/app/controladores/MascotaControlador.java
+++ src/main/java/com/patitas_perdidas/app/controladores/MascotaControlador.java
@@ -1,5 +1,6 @@
 package com.patitas_perdidas.app.controladores;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -7,6 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,110 +26,108 @@ import com.patitas_perdidas.app.servicios.MascotaServicio;
 public class MascotaControlador {
 	@Autowired
 	private MascotaServicio ms;
-	
-	//el metodo de encontrada y perdida podria ser uno solo pero cuando puse que retorne al index se mostraba mal.
+
+	// el metodo de encontrada y perdida podria ser uno solo pero cuando puse que
+	// retorne al index se mostraba mal.
 	@GetMapping("/registroencontrada")
 	public String registroencontrada() {
 		return "form-encontrada.html";
-	}	
-	
-
+	}
 
 	@PostMapping("/registroencontrada")
-	public String registroencontrada(ModelMap modelo,String nombre, String descripcion, String color, String raza, String tamaño,
-			Boolean encontrado, String fecha, String especie, String zona, MultipartFile archivo, RedirectAttributes redirAttrs){
+	public String registroencontrada(ModelMap modelo, String nombre, String descripcion, String color, String raza,
+			String tamaño, Boolean encontrado, String fecha, String especie, String zona, MultipartFile archivo,
+			RedirectAttributes redirAttrs) throws IOException {
 		try {
-		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
-		ms.crearMascota(nombre, descripcion, color, raza, tamaño, encontrado, date, especie, zona, archivo);
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+			ms.crearMascota(nombre, descripcion, color, raza, tamaño, encontrado, date, especie, zona, archivo);
 		} catch (MascotaExcepcion e) {
-		    redirAttrs.addFlashAttribute("error", e.getMessage());
-			return("redirect:./registroencontrada");
+			redirAttrs.addFlashAttribute("error", e.getMessage());
+			return ("redirect:./registroencontrada");
 			// Esto es para que aparezcan unas alertas al completar el formulario
 		} catch (ParseException e) {
-			 redirAttrs.addFlashAttribute("error", "Revise la fecha añadida");
-				return("redirect:./registroencontrada");
+			redirAttrs.addFlashAttribute("error", "Revise la fecha añadida");
+			return ("redirect:./registroencontrada");
 		}
-	    redirAttrs.addFlashAttribute("exito","Se añadio la mascota con exito");
-		return("redirect:./registroencontrada");
-	}	
-	
+		redirAttrs.addFlashAttribute("exito", "Se añadio la mascota con exito");
+		return ("redirect:./registroencontrada");
+	}
+
 	@GetMapping("/registroperdida")
 	public String registroperdida() {
 		return "form-perdida.html";
 	}
-	
-	
+
 	@PostMapping("/registroperdida")
-	public String registroperdida(ModelMap modelo,String nombre, String descripcion, String color, String raza, String tamaño,
-			Boolean encontrado, Date fecha, String especie, String zona, MultipartFile archivo, RedirectAttributes redirAttrs) throws ParseException {
+	public String registroperdida(ModelMap modelo, String nombre, String descripcion, String color, String raza,
+			String tamaño,Boolean encontrado, String fecha, String especie, String zona, MultipartFile archivo,
+			RedirectAttributes redirAttrs) throws ParseException, IOException {
 		try {
-		ms.crearMascota(nombre, descripcion, color, raza, tamaño, encontrado, fecha, especie, zona, archivo);
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+			ms.crearMascota(nombre, descripcion, color, raza, tamaño, encontrado, date, especie, zona, archivo);
 		} catch (MascotaExcepcion e) {
-		    redirAttrs.addFlashAttribute("error","Error al añadir la mascota");
-			return("redirect:./registroperdida");
+			redirAttrs.addFlashAttribute("error", "Error al añadir la mascota");
+			return ("redirect:./registroperdida");
 		}
-	    redirAttrs.addFlashAttribute("exito","Se añadio la mascota con exito");
-		return("redirect:./registroperdida");
+		redirAttrs.addFlashAttribute("exito", "Se añadio la mascota con exito");
+		return ("redirect:./registroperdida");
 
 	}
-	
-	
+
 	@GetMapping("/actualizar/{id}")
-	public String muestraActualiza(ModelMap modelo,@PathVariable String id) throws Exception {
-		Mascota m=ms.buscaPorId(id);
+	public String muestraActualiza(ModelMap modelo, @PathVariable String id) throws Exception {
+		Mascota m = ms.buscaPorId(id);
 		modelo.addAttribute(m);
 		return " ";
 	}
-	
+
 	@PostMapping("/actualizar/{id}")
-	public String actualiza(ModelMap modelo,String id, String nombre, String descripcion, String color, String raza, String tamaño,
-			Boolean encontrado, String fecha, String especie, String zona, MultipartFile archivo) throws ParseException, MascotaExcepcion {
-		Date date = new SimpleDateFormat("dd/MM/yyyy").parse(fecha);
+	public String actualiza(ModelMap modelo, String id, String nombre, String descripcion, String color, String raza,
+			String tamaño, Boolean encontrado, String fecha, String especie, String zona, MultipartFile archivo)
+			throws ParseException, MascotaExcepcion, IOException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
 		ms.modificarMascota(id, nombre, descripcion, color, raza, tamaño, encontrado, date, especie, zona, archivo);
 		modelo.put("Exito", "Actualoizacion exitosa");
 		return " ";
 	}
-	
+
 	@GetMapping("/eliminar/{id}")
 	public String elimina(ModelMap modelo, @PathVariable String id) {
 		ms.eliminarMascota(id);
 		return "";
 	}
-	
+
 	@GetMapping("/lista")
 	public String listar(ModelMap modelo) {
-		List<Mascota> muestraMascotas= ms.listarTodasMascotas();
+		List<Mascota> muestraMascotas = ms.listarTodasMascotas();
 		modelo.addAttribute("listaMascota", muestraMascotas);
 		return "";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listarActivos(ModelMap modelo) {
-		List<Mascota> muestraMascotas=ms.listarMascotasActivasPerdidas();
+		List<Mascota> muestraMascotas = ms.listarMascotasActivasPerdidas();
 		modelo.addAttribute("listaMascotasActivas", muestraMascotas);
 		return "";
 	}
-	
+
 	@GetMapping("/listarE")
 	public String listarActivos1(ModelMap modelo) {
-		List<Mascota> muestraMascotas=ms.listarMascotasActivasEncontradas();
+		List<Mascota> muestraMascotas = ms.listarMascotasActivasEncontradas();
 		modelo.addAttribute("listaMascotasEncontradas", muestraMascotas);
 		return "mascotasEncontradas.html";
 	}
-	
-	
-	
-	
+
 	@GetMapping("/listar/{raza}")
 	public String listarPorRaza(ModelMap modelo, @PathVariable String raza) {
-		List<Mascota> muestraMascotas=ms.listarMascotasPorRaza(raza);
+		List<Mascota> muestraMascotas = ms.listarMascotasPorRaza(raza);
 		modelo.addAttribute("listaMascotasxRaza", muestraMascotas);
 		return "";
 	}
-	
+
 	@GetMapping("/listar/{color}")
 	public String listarPorColor(ModelMap modelo, @PathVariable String color) {
-		List<Mascota> muestraMascotas=ms.listarMascotasColor(color);
+		List<Mascota> muestraMascotas = ms.listarMascotasColor(color);
 		modelo.addAttribute("listaMascotasxColor", muestraMascotas);
 		return "";
 	}
--- src/main/java/com/patitas_perdidas/app/entidades/Mascota.java
+++ src/main/java/com/patitas_perdidas/app/entidades/Mascota.java
@@ -3,9 +3,11 @@ package com.patitas_perdidas.app.entidades;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -17,8 +19,8 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 @Entity
 public class Mascota {
 	@Id
-	@GeneratedValue (generator = "uuid")
-	@GenericGenerator(name = "uuid",strategy = "uuid2")
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
 	private String id;
 	private String nombre;
 	private String descripcion;
@@ -26,96 +28,123 @@ public class Mascota {
 	private String raza;
 	private String tamaño;
 	private Boolean encontrado;
-	//para pasar de string (lo que toma del html a date	
+	// para pasar de string (lo que toma del html a date
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(iso = ISO.DATE)
 	private Date fecha;
 	private String especie;
 	private Boolean alta;
-	@OneToOne
-	private Foto foto;
+	@Lob
+	@Column(columnDefinition = "MEDIUMBLOB")
+	private String image;
 	private String zona;
 
-	
 	public Mascota() {
-	
+
 	}
-	public Foto getFoto() {
-		return foto;
-	}
-	public void setFoto(Foto foto) {
-		this.foto = foto;
-	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public String getRaza() {
 		return raza;
 	}
+
 	public void setRaza(String raza) {
 		this.raza = raza;
 	}
+
 	public String getTamaño() {
 		return tamaño;
 	}
+
 	public void setTamaño(String tamaño) {
 		this.tamaño = tamaño;
 	}
-	public String getZona() {
-		return zona;
-	}
-	public void setZona(String zona) {
-		this.zona = zona;
-	}
+
 	public Boolean getEncontrado() {
 		return encontrado;
 	}
+
 	public void setEncontrado(Boolean encontrado) {
 		this.encontrado = encontrado;
 	}
+
 	public Date getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
 	public String getEspecie() {
 		return especie;
 	}
+
 	public void setEspecie(String especie) {
 		this.especie = especie;
 	}
+
 	public Boolean getAlta() {
 		return alta;
 	}
+
 	public void setAlta(Boolean alta) {
 		this.alta = alta;
 	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getZona() {
+		return zona;
+	}
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(alta, color, descripcion, encontrado, especie, fecha, id, nombre, raza, tamaño, zona);
+		return Objects.hash(alta, color, descripcion, encontrado, especie, fecha, id, image, nombre, raza, tamaño,
+				zona);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -128,19 +157,16 @@ public class Mascota {
 		return Objects.equals(alta, other.alta) && Objects.equals(color, other.color)
 				&& Objects.equals(descripcion, other.descripcion) && Objects.equals(encontrado, other.encontrado)
 				&& Objects.equals(especie, other.especie) && Objects.equals(fecha, other.fecha)
-				&& Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre)
-				&& Objects.equals(raza, other.raza) && Objects.equals(tamaño, other.tamaño)
-				&& Objects.equals(zona, other.zona);
+				&& Objects.equals(id, other.id) && Objects.equals(image, other.image)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(raza, other.raza)
+				&& Objects.equals(tamaño, other.tamaño) && Objects.equals(zona, other.zona);
 	}
+
 	@Override
 	public String toString() {
 		return "Mascota [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", color=" + color
 				+ ", raza=" + raza + ", tamaño=" + tamaño + ", encontrado=" + encontrado + ", fecha=" + fecha
-				+ ", especie=" + especie + ", alta=" + alta + ", zona=" + zona + "]";
+				+ ", especie=" + especie + ", alta=" + alta + ", image=" + image + ", zona=" + zona + "]";
 	}
-	
-	
-	
-	
-	
+
 }
