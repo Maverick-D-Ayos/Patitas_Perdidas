253de9e72e012ca48312a9d12971b99acfd87df3
Tomas Vergara <tomas.vergara@outlook.com>
2021-11-17 22:27:53 -0300

    Funcion de olvido de contraseña terminada

--- src/main/java/com/patitas_perdidas/app/controladores/PersonaControlador.java
+++ src/main/java/com/patitas_perdidas/app/controladores/PersonaControlador.java
@@ -5,16 +5,21 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.patitas_perdidas.app.entidades.ConfirmationToken;
 import com.patitas_perdidas.app.entidades.Persona;
 import com.patitas_perdidas.app.excepciones.PersonaExcepcion;
 import com.patitas_perdidas.app.servicios.PersonaServicio;
@@ -22,7 +27,7 @@ import com.patitas_perdidas.app.servicios.PersonaServicio;
 @Controller
 @RequestMapping("/persona")
 public class PersonaControlador {
-
+	
 	@Autowired
 	private PersonaServicio personaServicio;
 
@@ -168,4 +173,89 @@ public class PersonaControlador {
 			return "redirect:/persona/perfil/{id}";
 		} 
 	}
+	
+	@RequestMapping(value="/forgot-password", method=RequestMethod.GET)
+    public ModelAndView displayResetPassword(ModelAndView modelAndView, Persona user) {
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("passwordOlvidada");
+        return modelAndView;
+    }
+
+    // Receive the address and send an email
+    @RequestMapping(value="persona/forgot-password", method=RequestMethod.POST)
+    public ModelAndView forgotUserPassword(ModelAndView modelAndView, Persona user) {
+        Persona existingUser = personaServicio.buscarPorEmail2(user.getMail());
+        System.out.println("Entramos sin saber porque");
+        if (existingUser != null) {
+        	
+            // Create token
+            ConfirmationToken confirmationToken = new ConfirmationToken(existingUser);
+
+            // Save it
+            personaServicio.guardarToken(confirmationToken);
+
+            // Create the email
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(existingUser.getMail());
+            mailMessage.setSubject("Complete Password Reset!");
+            mailMessage.setFrom("patitasperdidas.egg@gmail.com");
+            mailMessage.setText("To complete the password reset process, please click here: "
+              + "http://localhost:8080/persona/confirm-reset/"+confirmationToken.getConfirmationToken());
+
+            // Send the email
+            personaServicio.sendMailContraseña(mailMessage);
+
+            modelAndView.addObject("message", "Request to reset password received. Check your inbox for the reset link.");
+            modelAndView.setViewName("successForgotPassword");
+
+        } else {
+            modelAndView.addObject("message", "This email address does not exist!");
+            modelAndView.setViewName("error");
+        }
+        return modelAndView;
+    }
+    
+    // Endpoint to confirm the token
+    @RequestMapping(value="confirm-reset/{token}", method= {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView validateResetToken(ModelAndView modelAndView, @PathVariable String token) {
+       System.out.println(token+"Tendria que aparecer");
+    	ConfirmationToken token1 = personaServicio.confirmarToken(token);
+        System.out.println(token);
+        if (token != null) {
+            Persona user =personaServicio.buscarPorEmail2(token1.getUser().getMail());
+            try {
+				personaServicio.alta(user.getId());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            modelAndView.addObject("user", user);
+            modelAndView.addObject("mail", user.getMail());
+            modelAndView.setViewName("resetPassword");
+        } else {
+            modelAndView.addObject("message", "The link is invalid or broken!");
+            modelAndView.setViewName("error");
+        }
+        return modelAndView;
+    }
+    
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    // Endpoint to update a user's password
+    @RequestMapping(value = "confirm-reset/persona/reset-password", method = RequestMethod.POST)
+    public ModelAndView resetUserPassword(ModelAndView modelAndView, Persona user) {
+        System.out.println("Entro a la funcion");
+    	if (user.getMail() != null) {
+            // Use email to find user
+            Persona tokenUser = personaServicio.buscarPorEmail(user.getMail());
+            tokenUser.setClave(encoder.encode(user.getClave()));
+            personaServicio.guardarPersona(tokenUser);
+            modelAndView.addObject("message", "Password successfully reset. You can now log in with the new credentials.");
+            modelAndView.setViewName("successResetPassword");
+        } else {
+            modelAndView.addObject("message","The link is invalid or broken!");
+            modelAndView.setViewName("error");
+        }
+        return modelAndView;
+    }
 }
